6b076c5481ed98d7783063a36a0c982bcb3a9168
ericksebastian2003 <151951589+ericksebastian2003@users.noreply.github.com>
2024-11-22 14:13:14 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Libros_al_azar',
+  webDir: 'www'
+};
+
+export default config;
